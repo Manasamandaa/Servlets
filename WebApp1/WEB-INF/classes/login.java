ad6fb0842542000a1289ee2e566d252feea89195
Manasa Manda <121099961+Manasamandaa@users.noreply.github.com>
2024-07-22 09:25:50 +0530

    Update login.java

--- WebApp1/WEB-INF/classes/login.java
+++ WebApp1/WEB-INF/classes/login.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import jakarta.servlet.*;
+public class Login extends GenericServlet{
 
+    public void service(ServletRequest req ,ServletResponse res) throws ServletException, IOException
+    {
+        String uname=req.getParameter("username");
+        String pwd=req.getParameter("password");
+        if(uname.equals("Manasa")&&pwd.equals("123456"))
+       {
+            res.getWriter().print("<h1>Hello "+uname+"</h1>");
+       }
+       else
+       {    
+            res.getWriter().print("<h1>Invalid</h1>");
+       }
+    }
+}
